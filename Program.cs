e21ae64df752aad18c4fe813f81395da1e621cd6
OperatorIO-stock <vova.chigorin@yandex.com>
2025-03-14 13:43:45 +0300

    ok, let's goo

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿namespace kansasCityShuffle;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] numberArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+        Shuffle(numberArray);
+
+        Console.WriteLine(string.Join(", ", numberArray));
+    }
+
+    static void Shuffle(int[] valueArray)
+    {
+        Random random = new();
+
+        int tempArray;
+
+        for (int i = valueArray.Length - 1; i > 0; i--)
+        {
+            int randomRange = i + 1;
+            int j = random.Next(randomRange);
+
+            tempArray = valueArray[i];
+            valueArray[i] = valueArray[j];
+            valueArray[j] = tempArray; 
+        }
+    }
+}
